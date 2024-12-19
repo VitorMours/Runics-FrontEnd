acc9114757f9083d25f476ca724ebca9a9a84359
potato <mdiasoliveira55@gmail.com>
2024-12-19 16:25:55 -0300

    Creating the project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import axios from "axios";
+
+function App() {
+
+  const [data, setData] = useState();
+
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/dashboard/").then((response) => {
+    
+      console.log(response);
+      console.log(response.data);
+ 
+      setData(JSON.stringify(response.data));
+    });
+  },[])
+
+  return (
+    <div className="flex h-full w-full">
+      <Button className="content-center align-center justify-center" variant="contained">{data}</Button>
+    </div>
+  );
+}
+
+export default App;
